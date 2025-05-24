2d4563f1aee9780b0245400bf389d67e209b8a37
Mainul-Islam-07 <mainulislam@iut-dhaka.edu>
2025-05-24 15:25:28 +0600

    Changed pins, veroboard edition

--- Code_Files/Core/Inc/main.h
+++ Code_Files/Core/Inc/main.h
@@ -63,11 +63,11 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define W5500_RESET_Pin GPIO_PIN_2
+#define W5500_RESET_GPIO_Port GPIOA
 #define W5500_CS_Pin GPIO_PIN_4
 #define W5500_CS_GPIO_Port GPIOA
-#define W5500_RESET_Pin GPIO_PIN_13
-#define W5500_RESET_GPIO_Port GPIOB
-#define LED_L1_Pin GPIO_PIN_3
+#define LED_L1_Pin GPIO_PIN_9
 #define LED_L1_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
--- Code_Files/Core/Src/main.c
+++ Code_Files/Core/Src/main.c
@@ -242,24 +242,24 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(W5500_CS_GPIO_Port, W5500_CS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, W5500_RESET_Pin|W5500_CS_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, W5500_RESET_Pin|LED_L1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LED_L1_GPIO_Port, LED_L1_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : W5500_CS_Pin */
-  GPIO_InitStruct.Pin = W5500_CS_Pin;
+  /*Configure GPIO pins : W5500_RESET_Pin W5500_CS_Pin */
+  GPIO_InitStruct.Pin = W5500_RESET_Pin|W5500_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(W5500_CS_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : W5500_RESET_Pin LED_L1_Pin */
-  GPIO_InitStruct.Pin = W5500_RESET_Pin|LED_L1_Pin;
+  /*Configure GPIO pin : LED_L1_Pin */
+  GPIO_InitStruct.Pin = LED_L1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_Init(LED_L1_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
